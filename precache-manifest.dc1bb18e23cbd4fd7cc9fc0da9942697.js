939a9f68de6fb2c798c02036c4d3f7de3b65feab
HoangMinhTuan-ops <54118652+HoangMinhTuan-ops@users.noreply.github.com>
2020-04-04 19:38:59 +0200

    Updates

---
+++ precache-manifest.dc1bb18e23cbd4fd7cc9fc0da9942697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b31077cbdcbff49e9e89a427d862e0",
+    "url": "/HoangMinhTuan-ops/covdashboard/index.html"
+  },
+  {
+    "revision": "b663cd88a5d8ebef8f9d",
+    "url": "/HoangMinhTuan-ops/covdashboard/static/css/main.b1e40025.chunk.css"
+  },
+  {
+    "revision": "27806b99349162d80eb5",
+    "url": "/HoangMinhTuan-ops/covdashboard/static/js/2.923c9156.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HoangMinhTuan-ops/covdashboard/static/js/2.923c9156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b663cd88a5d8ebef8f9d",
+    "url": "/HoangMinhTuan-ops/covdashboard/static/js/main.ff531903.chunk.js"
+  },
+  {
+    "revision": "d4aee2b62fd8f59c99cf",
+    "url": "/HoangMinhTuan-ops/covdashboard/static/js/runtime-main.14b2ca40.js"
+  }
+]);
